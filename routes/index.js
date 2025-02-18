d9a4f5f664c7b5b3a2c9072d2f5347a9dee8046a
TajahH <167078781+TajahH@users.noreply.github.com>
2025-02-18 11:09:16 +0000

    messing about on pug

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', (req, res) => {
   res.render('index', { title: 'Express Pug Boilerplate' });
 });
 
+//Dynamic route
+router.get('/count/:id', (req, res) => {
+  console.log(req.params.id)
+  if (req.params.id > 5) {
+    res.render('index', { title: 'More than 5', brands: []})
+  }
+res.render('index', { title: req.params.id, brands: [] })
+})
+
+router.get('/Brands', (req, res) => {
+  const items = [
+    {name: 'Adidas'},
+    {name:'Nike'},
+    {name:'Puma'},
+    {name:'New Balance'},
+  ]
+  res.render('index', { title: 'Brands we sell: ', brands: items } )
+})
+
 module.exports = router;
